907e462d7a03851ff260b33b4e258139ad3eb914
DonAlPatino <valp@inbox.ru>
2023-06-03 13:21:06 +0300

    feat: 02-numbers-tasks. all tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -116,9 +116,22 @@ function getLinearEquationRoot(a, b = 0) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
-  // return Math.atan2(y1 - y2, x1 - x2);
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  // throw new Error('Not implemented');
+  const coord1 = {
+    x: x1,
+    y: y1,
+  };
+
+  const coord2 = {
+    x: x2,
+    y: y2,
+  };
+  const b = Math.abs(coord2.x - coord1.x);
+  const a = Math.abs(coord2.y - coord1.y);
+  let res = Math.atan2(a, b);
+  if (res < 0) { res += Math.PI; }
+  return res * 2;
 }
 
 /**
@@ -191,8 +204,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
@@ -200,7 +214,7 @@ function roundToPowerOfTen(/* num, pow */) {
  * See: https://en.wikipedia.org/wiki/Primality_test
  *
  * @param {number} n
- * @return {bool}
+ * @return {boolean}
  *
  * @example:
  *   4 => false
@@ -212,8 +226,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -231,8 +251,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  return Number(value) ? Number(value) : def;
 }
 
 module.exports = {
